9566163bf8b09f4e467a1148bcfb41d9213872dc
André Girol <andregirol@gmail.com>
2017-01-17 15:45:34 -0200

    Adding Torque manager script

--- torque/torqueman.sh
+++ torque/torqueman.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+# The Torque Version included in Ubuntu repositories is kinda strange. You need to star/stop
+# the services in the right order. Otherwise, some problems appear.
+
+# This script is intended to help you with it. Put it in your PATH and call the services as you want.
+
+
 start_torque(){
 
 	/etc/init.d/torque-server start
 	/etc/init.d/torque-scheduler start
-	/etc/init.d/torque-mom start
+#	/etc/init.d/torque-mom start
 }
 
 stop_torque(){
 
-	/etc/init.d/torque-mom stop
+#	/etc/init.d/torque-mom stop
 	/etc/init.d/torque-scheduler stop
 	/etc/init.d/torque-server stop
 }
