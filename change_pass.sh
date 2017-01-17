6a4a4526f94b0542b3b946bb24e049c3fcfcc932
André Girol <andregirol@gmail.com>
2017-01-17 14:33:39 -0200

    Refactor change_pass script
    Making it a little better to understand and adding some useful notes

--- change_pass.sh
+++ change_pass.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+#
+# This script is intended for being used with puppet for password generation.
+#
+# This script depends on the package "whois", which gives the 'mkpasswd' tool
 
-pass=$(mkpasswd -m sha-512 teste)
+#sudo apt-get update
+#sudo apt-get install whois
 
-puppet resource user fulano password=$pass
+# This command generates a random password.
+#alnum=`date +%s | sha256sum | base64 | head -c 32`
 
+echo "Type a password:"
 
+read pass
+
+# This command converts your password into a Unix Shadow type password using the
+# best encription available
+
+unix_pass=$(mkpasswd -m sha-512 $pass)
+
+echo -e "\nThe password generated is: \n "
+echo -e "=================================\n"
+echo $unix_pass
+echo -e "\n=================================\n"
